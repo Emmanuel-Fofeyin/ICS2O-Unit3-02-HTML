1bdd32a379287ad868ab5ae5c8242bea27a448a3
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-05 03:11:58 +0000

    fixed errors

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
 //
 // Created by: Emmanuel.Fofeyin
-// Created on:Tuesday March 29 2022
+// Created on: April  2022
 // This file contains the JS functions for index.html
 
 'use strict'
@@ -11,12 +11,12 @@
 function calculate () {
   // input
   const lLength = parseInt(document.getElementById('l-length').value)
-  const wWidth = parseInt(document.getElementById('b-length').value)
+  const wWidth = parseInt(document.getElementById('w-width').value)
   const hHeight = parseInt(document.getElementById('h-height').value)
 
   // process
-  const area = (lLength * wWidth * hHeight) / 3 * hLength
+  const volume = (lLength * wWidth * hHeight) / 3 
 
   // output
-  document.getElementById('area').innerHTML = 'Volume is: ' + volume + ' mm²'
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm²'
 }
